5d2b49cec24d7d55e99b11327ab2ec5c4b714213
Hannah <ha.al.bu@gmail.com>
2016-12-01 23:30:12 -0600

    use bradley's templates, fix links on index and detail pages

--- urls.py
+++ urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<recipe_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^user_page/$', views.user_page, name='user_page'),
+	url(r'^sign_in/$', views.sign_in, name='sign_in'),
+	url(r'^advanced_search/$', views.advanced_search, name='advanced_search'),
+	url(r'^readme/$', views.readme, name='readme'),
+	url(r'^create_account/$', views.create_account, name='create_account'),
 ]
--- views.py
+++ views.py
@@ -20,3 +20,23 @@ def detail(request, recipe_id):
 	except TestRecipe.DoesNotExist:
 		raise Http404("Recipe does not exist")
 	return render(request, 'cookbook/detail.html', {'recipe': recipe})
+
+
+def user_page(request):
+	return render(request, 'cookbook/user_page.html')
+
+
+def sign_in(request):
+	return render(request, 'cookbook/sign_in.html')
+
+
+def readme(request):
+	return render(request, 'cookbook/readme.html')
+
+
+def advanced_search(request):
+	return render(request, 'cookbook/advanced_search.html')
+
+
+def create_account(request):
+	return render(request, 'cookbook/create_user_account.html')
